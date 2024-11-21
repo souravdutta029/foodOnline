57a0fd0234c8316ea8286df3dc60b7c0025f863e
Sourav Dutta <duttasourav029@gmail.com>
2024-11-21 16:23:30 +0530

    homepage setup, postgre configuration, .env setup

--- foodOnline_main/views.py
+++ foodOnline_main/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Hello, world. You're at the foodOnline_main home.")
+    return render(request, 'home.html')
