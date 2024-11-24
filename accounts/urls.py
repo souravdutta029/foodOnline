9fa18684ecd164df2b0e97154e52fdb17c7e40c7
Sourav Dutta <duttasourav029@gmail.com>
2024-11-25 00:31:12 +0530

    Resgistration

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Register Links
     path('registerUser/', views.registerUser, name='registerUser'),
     path('registerVendor/', views.registerVendor, name='registerVendor'),
+    
+    # Login & logout Link
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('myAccount/', views.myAccount, name='myAccount'),
+    path('custDashboard/', views.custDashboard, name='custDashboard'),
+    path('vendorDashboard/', views.vendorDashboard, name='vendorDashboard'),
 ]
