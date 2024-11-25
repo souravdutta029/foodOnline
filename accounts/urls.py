edcd276eb904f5d7f02a71eed9ab3d25c667a293
Sourav Dutta <duttasourav029@gmail.com>
2024-11-25 18:37:53 +0530

    Token verification and Email configuration

--- accounts/urls.py
+++ accounts/urls.py
@@ -12,4 +12,12 @@ urlpatterns = [
     path('myAccount/', views.myAccount, name='myAccount'),
     path('custDashboard/', views.custDashboard, name='custDashboard'),
     path('vendorDashboard/', views.vendorDashboard, name='vendorDashboard'),
+    
+    # Email Verification
+    path('activate/<uidb64>/<token>/', views.activate, name="activate"),
+    
+    # Password Reset
+    path('forgot_password/', views.forgot_password, name="forgot_password"),
+    path('reset_password_validate/<uidb64>/<token>/', views.reset_password_validate, name="reset_password_validate"),
+    path('reset_password/', views.reset_password, name="reset_password"),
 ]
