80e175f4ac5e29276924e583de0dfffef7338818
Sourav Dutta <duttasourav029@gmail.com>
2024-11-30 18:34:34 +0530

    cart functionality

--- foodOnline_main/static/js/custom.js
+++ foodOnline_main/static/js/custom.js
@@ -68,3 +68,139 @@ function onPlaceChanged (){
     }
 
 }
+
+
+$(document).ready(function(){
+    // add to cart
+    $('.add_to_cart').on('click', function(e){
+        e.preventDefault();
+        food_id = $(this).attr('data-id');
+        url = $(this).attr('data-url');
+        data = {'food_id':food_id,};
+        
+        $.ajax({
+            type: 'GET',
+            url: url,
+            data: data,
+            success: function (response){
+                if(response.status == 'login_required'){
+                    swal.fire(response.message, "", "info").then(() => {
+                        window.location = '/login/';
+                    });
+                }else if(response.status == 'Failed') {
+                    swal.fire(response.message, "", "error");
+                } else {
+                    $('#cart-counter-navbar').html(response.cart_counter.cart_count);                  
+                    $('#qty-'+food_id).html(response.qty);
+
+                    // Subtotal, tax and grand total
+                    applyCartAmounts(
+                        response.cart_amount.subtotal, 
+                        response.cart_amount.tax, 
+                        response.cart_amount.grand_total
+                    );
+                }                  
+            }
+        })
+    })
+
+    // place the cart item quantity on load
+    $('.item-qty').each(function(){
+        var the_id = $(this).attr('id');
+        var qty = $(this).attr('data-qty');
+        $('#'+the_id).html(qty);
+    });
+
+    // decrease cart
+    $('.decrease-cart').on('click', function(e){
+        e.preventDefault();
+        food_id = $(this).attr('data-id');
+        url = $(this).attr('data-url');
+        cart_id = $(this).attr('id');
+        
+        $.ajax({
+            type: 'GET',
+            url: url,
+            success: function (response){
+                if(response.status == 'login_required'){
+                    swal.fire(response.message, "", "info").then(() => {
+                        window.location = '/login/';
+                    });
+                }else if(response.status == 'Failed') {
+                    swal.fire(response.message, "", "error");
+                } else {
+                    $('#cart-counter-navbar').html(response.cart_counter.cart_count);                  
+                    $('#qty-'+food_id).html(response.qty);
+
+                    // Subtotal, tax and grand total
+                    applyCartAmounts(
+                        response.cart_amount.subtotal, 
+                        response.cart_amount.tax, 
+                        response.cart_amount.grand_total
+                    );
+
+                    if(window.location.pathname == '/cart/'){
+                        removeCartItem(response.qty, cart_id);
+                        checkEmptyCart();
+                    }
+                }  
+            }
+        })
+    })
+
+    // Delete cart item
+    $('.delete_cart').on('click', function(e){
+        e.preventDefault();
+        cart_id = $(this).attr('data-id');
+        url = $(this).attr('data-url');
+        
+        $.ajax({
+            type: 'GET',
+            url: url,
+            success: function (response){
+                if(response.status == 'Failed') {
+                    swal.fire(response.message, "", "error");
+                } else {
+                    $('#cart-counter-navbar').html(response.cart_counter.cart_count);                  
+                    swal.fire(response.status, response.message, "success");
+
+                    // Subtotal, tax and grand total
+                    applyCartAmounts(
+                        response.cart_amount.subtotal, 
+                        response.cart_amount.tax, 
+                        response.cart_amount.grand_total
+                    );
+                    
+                    removeCartItem(0, cart_id);
+                    checkEmptyCart();
+                }  
+            }
+        })
+    })
+
+    // delete the cart element if qty is 0
+    function removeCartItem(cartItemQty, cart_id){
+        if (cartItemQty <= 0){
+            // remove the cart item element
+            document.getElementById('cart-item-'+cart_id).remove();
+        }
+    }
+
+    function checkEmptyCart(){
+        var cart_counter_navbar = document.getElementById('cart-counter-navbar').innerHTML;
+        if(cart_counter_navbar == 0){
+            document.getElementById('empty-cart').style.display = 'block';
+        }
+    }
+
+
+    // Apply cart amounts
+    function applyCartAmounts(subtotal, tax, grand_total){
+        if(window.location.pathname == '/cart/'){
+            $('#subtotal').html(subtotal);
+            $('#tax').html(tax);
+            $('#total').html(grand_total);
+        }
+    }
+
+});
--- foodOnline_main/urls.py
+++ foodOnline_main/urls.py
@@ -3,9 +3,14 @@ from django.urls import path, include
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from marketplace import views as marketplace_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('', include('accounts.urls')),
+    path('marketplace/', include('marketplace.urls')),
+    
+    # cart
+    path('cart/', marketplace_views.cart, name='cart'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
