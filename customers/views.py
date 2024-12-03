899c9c49f9fe658b590c414b194c071210ea8ba2
Sourav Dutta <duttasourav029@gmail.com>
2024-12-03 16:22:32 +0530

    Customer dashboard setup

--- customers/views.py
+++ customers/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth.decorators import login_required
 from accounts.forms import UserProfileForm, UserInfoForm
 from accounts.models import UserProfile
 from django.contrib import messages
+from orders.models import Order, OrderedFood
+import simplejson as json
 
 # Create your views here.
 
@@ -26,3 +28,26 @@ def cprofile(request):
         user_form = UserInfoForm(instance=request.user)
     context = {'profile_form': profile_form, 'user_form': user_form, 'profile': profile}
     return render(request, 'customers/cprofile.html', context)
+
+
+def my_orders(request):
+    orders = Order.objects.filter(user=request.user, is_ordered=True).order_by('-created_at')
+    context = {'orders': orders}
+    return render(request, 'customers/my_orders.html', context)
+
+
+def order_details(request, order_number):
+    try:
+        order = Order.objects.get(order_number=order_number , is_ordered=True)
+        ordered_food = OrderedFood.objects.filter(order=order)
+        
+        subtotal = 0
+        for item in ordered_food:
+            subtotal += item.quantity * item.price
+            
+        tax_data = json.loads(order.tax_data)
+        
+        context = {'order': order, 'ordered_food': ordered_food, 'subtotal': subtotal, 'tax_data': tax_data}
+        return render(request, 'customers/order_details.html', context)
+    except:
+        return redirect('customer')
